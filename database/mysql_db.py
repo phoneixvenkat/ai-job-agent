d1d651c69388d5be28675af5353aef0435551fb1
Venkatasaikumar Erla <venkatasaikumarerla@gmail.com>
2026-05-02 23:38:01 -0400

    Fix: Remove hardcoded MySQL password from default value

--- database/mysql_db.py
+++ database/mysql_db.py
@@ -12,7 +12,7 @@ DB_CONFIG = {
     "host":     os.getenv("MYSQL_HOST",     "localhost"),
     "port":     int(os.getenv("MYSQL_PORT", "3306")),
     "user":     os.getenv("MYSQL_USER",     "root"),
-    "password": os.getenv("MYSQL_PASSWORD", "jobpilot123"),
+    "password": os.getenv("MYSQL_PASSWORD", ""),
     "database": os.getenv("MYSQL_DATABASE", "jobpilot")
 }
 
